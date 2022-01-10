c79ad35c2ca62b12a73217efd6066fbaf8f297ad
Genne Liu <genneliu@gmail.com>
2022-01-10 11:06:31 -0800

    server file

--- backend/Server.js
+++ backend/Server.js
@@ -12,9 +12,5 @@ app.use(express.json()); //needing json
 
 app.use("/api/products", productRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ message: "API running..." });
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
