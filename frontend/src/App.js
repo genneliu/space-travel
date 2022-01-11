29a726be7e2ce0c9bceced23821409b78200912d
Genne Liu <genneliu@gmail.com>
2022-01-11 11:00:05 -0800

    side drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //screens
@@ -8,13 +9,16 @@ import CartScreen from "./screens/CartScreen";
 
 //components
 import Navbar from "./components/Navbar";
+import Backdrop from "./components/Backdrop";
+import SideDrawer from "./components/SideDrawer";
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
     <Router>
       <Navbar />
-      {/*  Sidedrawer*/}
-      {/*  Backdrop */}
+      <SideDrawer show={sideToggle} />
+      <Backdrop show={sideToggle} />
       <main>
         {" "}
         <Routes>
