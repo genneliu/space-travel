e3a7077234bc11cefa325beeb9ee0981ed59658d
Genne Liu <genneliu@gmail.com>
2022-01-12 09:59:25 -0800

    images

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 const Product = () => {
   return (
     <div className="product">
-      <img src={"/Assets/Images/space.webp"} alt="product name"></img>
+      <img
+        src={require(`../Assets/Images/space.webp`)}
+        alt="product name"
+      ></img>
       <div className="product__info">
         <p className="info__name"> </p>
         <p className="info__description">lorem ipsum </p>
         <p className="info__price">$499 </p>
         <Link to={`/product/${11}`} className="info__button">
-          {" "}
-          View{" "}
+          View
         </Link>
       </div>
     </div>
