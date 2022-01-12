5794253a237fa768cbb7be5b2ed8ad2467cad539
Genne Liu <genneliu@gmail.com>
2022-01-12 15:07:47 -0800

    redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from './redux/store'
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  <Provider />,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
