d5b180b7ec210d5aea5b09fc5c393af97d40d3d1
Genne Liu <genneliu@gmail.com>
2022-01-15 13:26:51 -0800

    shopping cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
 
 //actions
-import { addToCart } from "../redux/actions/cartActions";
+import { addToCart, removeFromCart } from "../redux/actions/cartActions";
 
 const CartScreen = () => {
   const dispatch = useDispatch();
@@ -16,6 +16,20 @@ const CartScreen = () => {
 
   const qtyChangeHandler = (id, qty) => {
     dispatch(addToCart(id, qty));
+  };
+
+  const removeHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const getCartSubTotal = () => {
+    return cartItems
+      .reduce((price, item) => item.price * item.qty + price, 0)
+      .toFixed(2);
   };
 
   return (
@@ -29,14 +43,18 @@ const CartScreen = () => {
           </div>
         ) : (
           cartItems.map((item) => (
-            <CartItem item={item} qtyChangeHandler={qtyChangeHandler} />
+            <CartItem
+              item={item}
+              qtyChangeHandler={qtyChangeHandler}
+              removeHandler={removeHandler}
+            />
           ))
         )}
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
-          <p> Subtotal (0) Items </p>
-          <p> $499 </p>
+          <p> Subtotal ({getCartCount()}) Items </p>
+          <p> ${getCartSubTotal()} </p>
         </div>
         <div>
           <button> Proceed to Checkout</button>
