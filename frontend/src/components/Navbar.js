a5ca359e34372c71f66b93a64c0e6efba1364931
Genne Liu <genneliu@gmail.com>
2022-01-15 13:35:58 -0800

    local storage

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,15 @@
 //react17 does not need react import anymore
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = ({ click }) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+  };
   return (
     <div className="navbar">
       <div className="navbar__logo">
@@ -17,7 +24,7 @@ const Navbar = ({ click }) => {
             <span>
               {" "}
               Cart
-              <span className="cartlogo__badge">0</span>{" "}
+              <span className="cartlogo__badge">{getCartCount()}</span>{" "}
             </span>
           </Link>
         </li>
