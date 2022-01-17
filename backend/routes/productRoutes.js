d6921882def9932a053728fb387db4f543c25b5e
Genne Liu <genneliu@gmail.com>
2022-01-17 12:31:46 -0800

    product routes backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ const {
 // @desc get all products from db
 // @route get /api/products
 //@ access public
-router.get("/shop", getAllProducts);
+router.get("/", getAllProducts);
 
 // @desc get all products from db
 // @route get /api/products
