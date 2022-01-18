90ac4ae789de4622665f7317f89a1080ebbb7cba
Genne Liu <genneliu@gmail.com>
2022-01-18 11:34:20 -0800

    fonts

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const HomeScreen = () => {
   return (
     <div>
       <h1> Welcome </h1>
-      <button> Store </button>
-      <button> Contact Us </button>
+      <Link to="/shop">
+        {" "}
+        <button> Shop </button>{" "}
+      </Link>
+      <Link to="/contact">
+        <button> Contact Us </button>{" "}
+      </Link>
     </div>
   );
 };
