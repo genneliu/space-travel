be2ad2f1317aee9c80447610cc653e40decd83b0
Genne Liu <genneliu@gmail.com>
2022-01-18 11:09:02 -0800

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,6 +5,9 @@ const Footer = () => {
   return (
     <div>
       {" "}
+      <h3> Frontier </h3>
+      <p> Transport Union International </p>
+      <p> 117 Sol Hall, Office #5, Medina Station </p>
       <Link to="/contact" className="contact__link">
         Contact Us
       </Link>
