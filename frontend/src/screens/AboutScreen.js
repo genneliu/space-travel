a10f9c758158e519e8f1c66155719822f012536b
Genne Liu <genneliu@gmail.com>
2022-01-20 10:21:12 -0800

    fonts global

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -6,6 +6,11 @@ const AboutScreen = () => {
       About Us
       <h1> Once upon a time... </h1>
       <h2> The stars were out of humanity's grasp </h2>
+      <p>
+        {" "}
+        Font Protomolecule:
+        https://www.reddit.com/r/TheExpanse/comments/o9yk1d/protomolecule_font_062021/{" "}
+      </p>
     </div>
   );
 };
