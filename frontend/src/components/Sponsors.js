b218b02165b5152862e610961c74210758bdff88
Genne Liu <genneliu@gmail.com>
2022-01-27 17:47:24 -0700

    component css

--- frontend/src/components/Sponsors.js
+++ frontend/src/components/Sponsors.js
@@ -3,15 +3,19 @@ import "./Sponsors.css";
 
 //images
 import MCRN from "../Assets/Images/MCRN.jpg";
+import OPA from "../Assets/Images/OPA.webp";
+import UN from "../Assets/Images/UN.webp";
 
 const Sponsors = () => {
   return (
     <div className="sponsor__logos">
       <image src={MCRN} alt="MCRN Logo"></image>
-      <image></image>
-      <image></image>
+      <image src={OPA} alt="OPA"></image>
+      <image src={UN} alt="UN"></image>
     </div>
   );
 };
 
 export default Sponsors;
+
+// add css
