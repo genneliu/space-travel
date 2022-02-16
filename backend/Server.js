325edb06bc91e3e19644eda8391e1b75708702b0
Genne Liu <genneliu@gmail.com>
2022-02-16 11:27:00 -0800

    server & procfile

--- backend/Server.js
+++ backend/Server.js
@@ -15,5 +15,11 @@ app.use("/api/products", productRoutes);
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
